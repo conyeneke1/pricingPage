fb7ccdf91969d965968db2a27b0de9940f68d0ad
conyeneke1 <conyeneke1@gmail.com>
2022-05-24 16:26:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let headerBtn1 = document.querySelector('.btn-1');
+let headerBtn2 = document.querySelector('.btn-2');
+let showcaseYearly = document.querySelector('.showcase-yearly');
+let showcaseMonthly = document.querySelector('.showcase-monthly');
+
+headerBtn2.addEventListener('click', function () {
+    showcaseYearly.style.display = 'flex';
+    showcaseMonthly.style.display = 'none';
+    headerBtn2.classList.add('color1');
+    headerBtn2.classList.add('color4');
+    headerBtn1.classList.add('color2');
+    headerBtn1.classList.add('color3');
+})
+
+headerBtn1.addEventListener('click', function () {
+    showcaseMonthly.style.display = 'flex';
+    showcaseYearly.style.display = 'none';
+    headerBtn1.classList.remove('color2');
+    headerBtn1.classList.remove('color3');
+    headerBtn2.classList.remove('color1');
+    headerBtn2.classList.remove('color4');
+})
